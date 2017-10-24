e1fa2d9be9b57a7db60aca56efdd88f01e6e700d
jdeabreu <jmanueldeabreum@gmail.com>
2017-10-23 22:29:02 -0400

    Update formalities_master.rb

--- app/models/formalities_master.rb
+++ app/models/formalities_master.rb
@@ -1,4 +1,4 @@
 class FormalitiesMaster < ActiveRecord::Base
 	has_many :formalities_documents
-	has_many :documents, through: :formalities_documents
+	has_many :transfer_documents, through: :formalities_documents
 end

883ba4df6be26920e8b6071b88996a22b5c98af8
jdeabreu <jmanueldeabreum@gmail.com>
2017-10-24 01:14:57 -0400

    Update 20170213050955_create_formalities_documents.rb

--- db/migrate/20170213050955_create_formalities_documents.rb
+++ db/migrate/20170213050955_create_formalities_documents.rb
@@ -2,7 +2,7 @@ class CreateFormalitiesDocuments < ActiveRecord::Migration
   def change
     create_table :formalities_documents do |t|
       t.references :formalities_master, index: true, foreign_key: true
-      t.references :document, index: true, foreign_key: true
+      t.references :transfer_document, index: true, foreign_key: true
 
       t.timestamps null: false
     end

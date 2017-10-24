434642d67eafb62594808a66453757e953b2c635
jdeabreu <jmanueldeabreum@gmail.com>
2017-10-24 01:12:35 -0400

    Update and rename 20170203022358_create_documents.rb to 20170203022358_create_transfer_documents.rb

--- db/migrate/20170203022358_create_transfer_documents.rb
+++ db/migrate/20170203022358_create_transfer_documents.rb
@@ -1,6 +1,6 @@
-class CreateDocuments < ActiveRecord::Migration
+class CreateTransferDocuments < ActiveRecord::Migration
   def change
-    create_table :documents do |t|
+    create_table :transfer_documents do |t|
       t.string :name
       t.integer :status
 

069cfa9a6f294959e0917e39f2f3b2d6fc440ebf
jdeabreu <jmanueldeabreum@gmail.com>
2017-10-24 01:11:27 -0400

    Update 20161215220041_create_reports.rb

--- db/migrate/20161215220041_create_reports.rb
+++ db/migrate/20161215220041_create_reports.rb
@@ -3,7 +3,7 @@ class CreateReports < ActiveRecord::Migration
     create_table :reports do |t|
       t.references :user
       t.references :document
-      t.references :jointplan
+      #t.references :jointplan
       t.integer :applicant_id
 
       t.timestamps null: false

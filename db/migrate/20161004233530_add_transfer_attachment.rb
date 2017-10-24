ceef6d045ec03812d4611276092b592368a0ba67
jdeabreu <jmanueldeabreum@gmail.com>
2017-10-24 02:19:20 -0400

    Update and rename 20161004233530_add_transfer_attachmentfield.rb to 20161004233530_add_transfer_attachment.rb

--- db/migrate/20161004233530_add_transfer_attachment.rb
+++ db/migrate/20161004233530_add_transfer_attachment.rb
@@ -1,4 +1,4 @@
-class AddTransferAttachmentfield < ActiveRecord::Migration
+class AddTransferAttachment < ActiveRecord::Migration
   def change
 
     add_transfer_attachment :transfer_attachments,:file

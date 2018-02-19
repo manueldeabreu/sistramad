2086186aebb62fdfec55d41991c16a002b78e804
jesusmanuel <jmanueldeabreum@gmail.com>
2018-02-18 23:12:58 -0430

    constant changes

--- app/models/reference_list.rb
+++ app/models/reference_list.rb
@@ -10,10 +10,16 @@ class ReferenceList < ApplicationRecord
   has_many   :employees_faculty, :class_name => 'Employee', :foreign_key => 'faculty_classification_id'
   has_many   :employees_department, :class_name => 'Employee', :foreign_key => 'department_classification_id'
 
-  scope :type_employee, -> { where(:reference_id => ReferenceConstant::TYPE_EMPLOYEE) }
-  scope :scale_classification, -> { where(:reference_id => ReferenceConstant::SCALE_CLASSIFICATION) }
-  scope :scale_category, -> { where(:reference_id => ReferenceConstant::SCALE_CATEGORY) }
-  scope :dedication_classification, -> { where(:reference_id => ReferenceConstant::DEDICATION_CLASSIFICATION) }
-  scope :faculty_classification, -> { where(:reference_id => ReferenceConstant::FACULTY_CLASSIFICATION) }
-  scope :department_classification, -> { where(:reference_id => ReferenceConstant::DEPARTMENT_CLASSIFICATION) }
+  #scope :type_employee, -> { where(:reference_id => ReferenceConstant::TYPE_EMPLOYEE) }
+  #scope :scale_classification, -> { where(:reference_id => ReferenceConstant::SCALE_CLASSIFICATION) }
+  #scope :scale_category, -> { where(:reference_id => ReferenceConstant::SCALE_CATEGORY) }
+  #scope :dedication_classification, -> { where(:reference_id => ReferenceConstant::DEDICATION_CLASSIFICATION) }
+  #scope :faculty_classification, -> { where(:reference_id => ReferenceConstant::FACULTY_CLASSIFICATION) }
+  #scope :department_classification, -> { where(:reference_id => ReferenceConstant::DEPARTMENT_CLASSIFICATION) }
+  scope :type_employee, -> { where(:reference_id => 5) }
+  scope :scale_classification, -> { where(:reference_id => 3) }
+  scope :scale_category, -> { where(:reference_id => 4) }
+  scope :dedication_classification, -> { where(:reference_id => 11) }
+  scope :faculty_classification, -> { where(:reference_id => 8) }
+  scope :department_classification, -> { where(:reference_id => 9) }
 end
